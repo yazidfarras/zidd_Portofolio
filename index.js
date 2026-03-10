d3b670cd929b5bd70972aace2d504abe14e76e5e
Yazid Farras <fadillahyazid444@email.com>
2026-03-10 13:24:19 +0430

    update responsive

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ document.addEventListener("mousemove",(e)=>{
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
+
+
+
+setInterval(() => {
+    textElement.classList.add("slide-out");
+
+    setTimeout(() => {
+        index = (index + 1) % texts.length;
+        textElement.textContent = texts[index];
+        textElement.classList.remove("slide-out");
+    }, 500);
+
+}, 2000);
